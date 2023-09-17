70f33aa315e0690dba610bb1b7c8da4c4d7faca7
Osama Debsa <osamas1616@gmail.com>
2023-09-17 23:29:21 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const NotesApp());
+}
+
+class NotesApp extends StatelessWidget {
+  const NotesApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Notes App',
+      home: Scaffold(),
+    );
+  }
+}
