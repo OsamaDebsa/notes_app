764179c18d4a0339a7fbd742d43993b820b66032
Osama Debsa <osamas1616@gmail.com>
2023-09-18 23:08:15 +0300

    add floating action button

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -6,8 +6,13 @@ class NotesView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: NotesViewBody(),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        // backgroundColor: Colors.black54,
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: const NotesViewBody(),
     );
   }
 }
