3139af253058060126071aa8432cf5b285842597
Osama Debsa <osamas1616@gmail.com>
2023-09-21 23:15:09 +0300

    create add note cubit

---
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/models/note_model.dart';
+
+class AddNoteCubit extends Cubit<NoteModel> {
+  AddNoteCubit(super.initialState);
+}
