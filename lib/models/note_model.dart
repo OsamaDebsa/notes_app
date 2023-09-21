679e4a51064201e0690b8423023df44c53e6cc01
Osama Debsa <osamas1616@gmail.com>
2023-09-21 18:25:57 +0300

    create type adapter for note model

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,8 +1,21 @@
-class NoteModel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+// after write part''; it give with me error and run this line in trminal :
+// flutter packages pub run build_runner build
+
+@HiveType(typeId: 0)
+class NoteModel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String content;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
+
   NoteModel({
     required this.title,
     required this.content,
