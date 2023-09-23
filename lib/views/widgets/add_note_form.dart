04a9bf393fd7254c3c3e40484f67a503df5ededb
Osama Debsa <osamas1616@gmail.com>
2023-09-23 18:56:49 +0300

    change add note bottom sheet Loading

--- lib/views/widgets/add_note_form.dart
+++ lib/views/widgets/add_note_form.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
+import 'package:notes_app/cubits/add_note_cubit/add_note_state.dart';
 import 'package:notes_app/models/note_model.dart';
 import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_field.dart';
@@ -18,6 +19,7 @@ class _AddNoteFormState extends State<AddNoteForm> {
   final GlobalKey<FormState> formkey = GlobalKey();
   AutovalidateMode autovalidateMode = AutovalidateMode.disabled;
   String? title, content;
+
   @override
   Widget build(BuildContext context) {
     return Form(
@@ -41,22 +43,27 @@ class _AddNoteFormState extends State<AddNoteForm> {
             },
           ),
           const SizedBox(height: 40),
-          CustomButton(
-              buttonTitle: "Add",
-              onTab: () {
-                if (formkey.currentState!.validate()) {
-                  formkey.currentState!.save();
-                  var noteModel = NoteModel(
-                      title: title!,
-                      content: content!,
-                      date: DateTime.now().toString(),
-                      color: Colors.blue.value);
-                  BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
-                } else {
-                  autovalidateMode = AutovalidateMode.always;
-                  setState(() {});
-                }
-              }),
+          BlocBuilder<AddNoteCubit, AddNoteState>(
+            builder: (context, state) {
+              return CustomButton(
+                  buttonTitle: "Add",
+                  isLoading: state is AddNoteLoading ? true : false,
+                  onTab: () {
+                    if (formkey.currentState!.validate()) {
+                      formkey.currentState!.save();
+                      var noteModel = NoteModel(
+                          title: title!,
+                          content: content!,
+                          date: DateTime.now().toString(),
+                          color: Colors.blue.value);
+                      BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
+                    } else {
+                      autovalidateMode = AutovalidateMode.always;
+                      setState(() {});
+                    }
+                  });
+            },
+          ),
           const SizedBox(height: 20),
         ],
       ),
--- lib/views/widgets/custom_button.dart
+++ lib/views/widgets/custom_button.dart
@@ -3,9 +3,13 @@ import 'package:notes_app/constants.dart';
 
 class CustomButton extends StatelessWidget {
   const CustomButton(
-      {super.key, required this.buttonTitle, required this.onTab});
+      {super.key,
+      required this.buttonTitle,
+      required this.onTab,
+      this.isLoading = false});
   final String buttonTitle;
   final VoidCallback onTab;
+  final bool isLoading;
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -18,11 +22,21 @@ class CustomButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
         ),
         child: Center(
-          child: Text(
-            buttonTitle,
-            style: const TextStyle(
-                color: Colors.black, fontSize: 22, fontWeight: FontWeight.w600),
-          ),
+          child: isLoading
+              ? const SizedBox(
+                  height: 25,
+                  width: 25,
+                  child: CircularProgressIndicator(
+                    color: Colors.black,
+                  ),
+                )
+              : Text(
+                  buttonTitle,
+                  style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 22,
+                      fontWeight: FontWeight.w600),
+                ),
         ),
       ),
     );
